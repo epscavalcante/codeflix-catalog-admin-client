e8124869157f50f925fc0951dc76458d05c18f4d
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-03-21 15:39:43 -0400

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/codeflix-catalog-admin-client/',
   plugins: [vue()],
   server:{
     host: '0.0.0.0',
