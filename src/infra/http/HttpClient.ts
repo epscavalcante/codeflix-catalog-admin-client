74f5cd972ccc656c688ee638e7d8813db025935a
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2024-04-12 19:10:45 -0400

    fixes

--- src/infra/http/HttpClient.ts
+++ src/infra/http/HttpClient.ts
@@ -1,6 +1,6 @@
 export default interface HttpClient {
   get(url: string): Promise<any>;
-  post<R = any>(url: string, body?: any, config?: any): Promise<R>;
+  post<R = any>(url: string, body?: any, config?: any): Promise<any>;
   patch(url: string, body: any): Promise<any>;
   delete(url: string): Promise<any>;
 }
